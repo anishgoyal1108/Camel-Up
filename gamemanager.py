3b2ba0e9fd68e381aa9b929707417b0e431dfa17
Anish G. <anishgoyal1108@gmail.com>
2024-07-26 17:57:34 -0400

    initial commit

---
+++ gamemanager.py
@@ -0,0 +1,17 @@
+from player import Player
+
+
+def GameManager():
+    def __init__(self, Player1=Player, Player2=Player):
+        self.cards = {
+            "red": [5, 3, 2, 2],
+            "green": [5, 3, 2, 2],
+            "blue": [5, 3, 2, 2],
+            "yellow": [5, 3, 2, 2],
+            "purple": [5, 3, 2, 2],
+        }
+        self.dice = ["red", "green", "blue", "yellow", "purple"]
+        pass
+
+    def play_game(self):
+        pass
---
+++ player.py
@@ -0,0 +1,14 @@
+from gamemanager import GameManager
+
+
+class Player:
+    def __init__(self):
+        self.coins = 3
+        self.cards = {"red": [], "green": [], "blue": [], "yellow": [], "purple": []}
+        pass
+
+    def roll():
+        pass
+
+    def take_bet():
+        pass
