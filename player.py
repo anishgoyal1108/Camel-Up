b7571d9b094990eb7f0ba590a1395d57f78001ee
Anish G. <anishgoyal1108@gmail.com>
2024-07-29 11:06:03 -0400

    feat: add Player.roll

--- player.py
+++ player.py
@@ -1,14 +1,16 @@
 from gamemanager import GameManager
+import random
 
 
 class Player:
     def __init__(self):
         self.coins = 3
         self.cards = {"red": [], "green": [], "blue": [], "yellow": [], "purple": []}
-        pass
 
-    def roll():
-        pass
+    def roll(self, gm: GameManager) -> tuple:
+        pick = random.choice(gm.dice)
+        gm.dice.remove(pick)
+        return (pick, random.choice([1, 2, 3])) if bool(self.dice) else ()
 
-    def take_bet():
+    def take_bet(self, gm: GameManager):
         pass
