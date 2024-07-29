45f69b896896f4f2c3d2c175715a2beef50737b1
AdelineG218 <47432123+AdelineG218@users.noreply.github.com>
2024-07-29 11:32:33 -0400

    added update_score function

--- gamemanager.py
+++ gamemanager.py
@@ -1,9 +1,7 @@
 from player import Player
-import colorama
-
 
 def GameManager():
-    def __init__(self, Player1=Player, Player2=Player):
+    def __init__(self, Player1=Player(), Player2=Player()):
         self.cards = {
             "red": [5, 3, 2, 2],
             "green": [5, 3, 2, 2],
@@ -14,12 +12,31 @@ def GameManager():
         self.dice = ["red", "green", "blue", "yellow", "purple"]
         self.current_player = Player1
         self.board = [[] * 16]
+        
+        self.player_scores = [0, 0]
+        self.winning_camel = ""
+        self.second_camel = ""
+        self.players = [Player1, Player2]
+        self.player2 = Player2
 
     def play_game(self):
         pass
 
     def display_game(self):
         pass
+
+    def update_score(self) -> None:
+        for p in range(2):
+            for camel in self.players[p].cards:
+                if camel == self.winning_camel:
+                    for card in self.players[p].cards[self.winning_camel]:
+                        self.player_scores[p] += card
+                elif camel == self.second_camel:
+                    for card in self.players[p].cards[self.second_camel]:
+                        self.player_scores[p] += 1
+                else:
+                    for card in self.players[p].cards[camel]:
+                        self.player_scores[p] -= 1
 
     def display_stats(self):
         pass
