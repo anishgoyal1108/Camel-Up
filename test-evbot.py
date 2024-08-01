1c9a491c392a956651e450ab41fd7e2f6de0b1c1
AdelineG218 <greene.adeline08@gmail.com>
2024-07-31 23:08:59 -0400

    hump day!

--- test-evbot.py
+++ test-evbot.py
@@ -1,2 +1,16 @@
 import unittest
 from evbot import EVBot
+from gamemanager import GameManager
+
+class TestSimulateMove(unittest.TestCase):
+    def setUp(self) -> None:
+        self.game = GameManager().init_camels()
+        self.bot = EVBot(self.game)
+
+    def test_0(self):
+        """Test return datatype"""
+        actual = self.bot.simulate_move("red", 1, self.game.board)
+        self.assertIsInstance(actual, tuple[list[list], str, str])
+
+if __name__ == "__main__":
+    unittest.main()
